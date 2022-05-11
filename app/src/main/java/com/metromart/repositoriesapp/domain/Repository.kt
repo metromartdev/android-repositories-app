682ad7a698a1a09622f15854e44c4ea12236ec4e
May Ann Palencia <mayann.palencia@gmail.com>
2022-05-11 15:13:22 +0800

    fix: removing unusued attributes and plugins

--- app/src/main/java/com/metromart/repositoriesapp/domain/Repository.kt
+++ app/src/main/java/com/metromart/repositoriesapp/domain/Repository.kt
@@ -1,55 +1,9 @@
 package com.metromart.repositoriesapp.domain
 
-import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class Repository(
     @SerializedName("id") var id: Int,
-    @SerializedName("node_id") var nodeId: String,
     @SerializedName("name") var name: String,
-    @SerializedName("full_name") var fullName: String,
-    @SerializedName("private") var private: Boolean,
-    @SerializedName("owner") var owner: Owner,
-    @SerializedName("html_url") var htmlUrl: String,
-    @SerializedName("description") var description: String,
-    @SerializedName("fork") var fork: Boolean,
-    @SerializedName("url") var url: String,
-    @SerializedName("forks_url") var forksUrl: String,
-    @SerializedName("keys_url") var keysUrl: String,
-    @SerializedName("collaborators_url") var collaboratorsUrl: String,
-    @SerializedName("teams_url") var teamsUrl: String,
-    @SerializedName("hooks_url") var hooksUrl: String,
-    @SerializedName("issue_events_url") var issueEventsUrl: String,
-    @SerializedName("events_url") var eventsUrl: String,
-    @SerializedName("assignees_url") var assigneesUrl: String,
-    @SerializedName("branches_url") var branchesUrl: String,
-    @SerializedName("tags_url") var tagsUrl: String,
-    @SerializedName("blobs_url") var blobsUrl: String,
-    @SerializedName("git_tags_url") var gitTagsUrl: String,
-    @SerializedName("git_refs_url") var gitRefsUrl: String,
-    @SerializedName("trees_url") var treesUrl: String,
-    @SerializedName("statuses_url") var statusesUrl: String,
-    @SerializedName("languages_url") var languagesUrl: String,
-    @SerializedName("stargazers_url") var stargazersUrl: String,
-    @SerializedName("contributors_url") var contributorsUrl: String,
-    @SerializedName("subscribers_url") var subscribersUrl: String,
-    @SerializedName("subscription_url") var subscriptionUrl: String,
-    @SerializedName("commits_url") var commitsUrl: String,
-    @SerializedName("git_commits_url") var gitCommitsUrl: String,
-    @SerializedName("comments_url") var commentsUrl: String,
-    @SerializedName("issue_comment_url") var issueCommentUrl: String,
-    @SerializedName("contents_url") var contentsUrl: String,
-    @SerializedName("compare_url") var compareUrl: String,
-    @SerializedName("merges_url") var mergesUrl: String,
-    @SerializedName("archive_url") var archiveUrl: String,
-    @SerializedName("downloads_url") var downloadsUrl: String,
-    @SerializedName("issues_url") var issuesUrl: String,
-    @SerializedName("pulls_url") var pullsUrl: String,
-    @SerializedName("milestones_url") var milestonesUrl: String,
-    @SerializedName("notifications_url") var notificationsUrl: String,
-    @SerializedName("labels_url") var labelsUrl: String,
-    @SerializedName("releases_url") var releasesUrl: String,
-    @SerializedName("deployments_url") var deploymentsUrl: String
-) : Parcelable
+    @SerializedName("description") var description: String
+)
